81db485916683f95f37e304078f3bb525b81bd39
Vincent <23vincent86@gmail.com>
2020-07-30 11:11:18 +0200

    first function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+$('#ad-text').keydown(function() {
+    if (event.which == 13 || event.keyCode == 13) {
+        inviomessaggio();
+    }
+});
 
+
+// ****FUNZIONI****
+
+function inviomessaggio(){
+    var valore = $('#ad-text').val();
+    var clone = $('.template .message').clone();
+    clone.addClass('sent');
+    clone.find('.message-text').append(valore);
+    clone.find('.message-time').append('16:40');
+    $('.chat').append(clone);
+    // setTimeout(rispostaAutomatica,2000);
+};
